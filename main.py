2bf79641e7da635d4472c0be5488b8b4acc8fdfe
SergeiMukha <sergeimushonok@gmail.com>
2024-07-10 10:35:19 +0300

    parser finished

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+import time
+import random
 
-from parser import Parser
+from parser import RdoParser
+from excel_controller import ExcelController
+
+
+def save_current_row(row):
+    with open("current_row.txt", "w") as f:
+        f.write(str(row))
+
+def get_current_row():
+    with open("current_row.txt") as f:
+        return int(f.read())
 
 
 def main():
-    parser: Parser = Parser()
+    parser: RdoParser = RdoParser()
 
-    link = parser.get_part_link("re539279")
-    
-    parser.get_page_data(link, "re539279")
+    excelController = ExcelController("RDO.xlsx", get_current_row())
+    max_row = excelController.max_row
+
+    while excelController.row <= max_row:
+        art = str(excelController.sheet[f"A{excelController.row}"].value).replace(",", "")
+        print(excelController.row, art)
+
+        link = parser.get_part_link(art)
+        if(not link):
+            excelController.row += 1
+            continue
+
+        data = parser.get_page_data(link, art)
+
+        excelController.sheet[f"B{excelController.row}"] = data["models"] or ""
+        excelController.sheet[f"C{excelController.row}"] = "" if data["img"] else "Пусто"
+
+        excelController.save_document()
+
+        save_current_row(excelController.row)
+
+        excelController.row += 1
+
+        time.sleep(random.randint(1, 2))
 
 if __name__ == "__main__":
     main()
--- parser.py
+++ parser.py
@@ -8,9 +8,12 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
-class Parser:
+class RdoParser:
     def __init__(self):
         self.driver = webdriver.Chrome = webdriver.Chrome()
+
+        self.driver.get("https://www.rdoequipment.com")
+        self._accept_cookies()
 
     def get_part_link(self, art):
         self.driver.get(f"https://www.rdoequipment.com/search?query={art}")
@@ -20,7 +23,7 @@ class Parser:
         
         link = self.driver.find_element(By.CLASS_NAME, "part_item").find_element(By.TAG_NAME, "a").get_attribute("href")
 
-        if(art not in link): return None
+        if(art.lower() not in link): return None
         else: return link
 
     def get_page_data(self, link, art):
@@ -32,9 +35,27 @@ class Parser:
             self._download_photo(img_link, art.replace("/", "-"))
         except TimeoutException: img_link = None
 
-        print(img_link)
+        try:
+            models_element = self.driver.find_element(By.XPATH, "//div[@id='product-info']/p")
 
-    
+            if("fits on" not in models_element.text.lower()): raise Exception()
+            models = ", ".join(models_element.text.split("\n")[1:])
+        except Exception as ex:
+            models = None
+
+        return {
+            "models": models,
+            "img": img_link
+        }
+
+
+    def _accept_cookies(self):
+        try:
+            accept_button = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "onetrust-accept-btn-handler")))
+            accept_button.click()
+        except TimeoutError:
+            print("Couldn't accept cookies")
+
     # Downloads photo from the page
     def _download_photo(self, link: str, filename: str):
         img_data = requests.get(link).content
